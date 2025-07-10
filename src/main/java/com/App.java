3c63c14775327028e0fc106e7355187b523fa9e7
2021wc86413 <2021wc86413@wilp.bits-pilani.ac.in>
2025-07-10 17:06:43 +0530

    Update App.java

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,7 +1,7 @@
 package com.example;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins CI!");
+        System.out.println("Hello, Jenkins CI! Sunny this side.");
     }
 }
 
